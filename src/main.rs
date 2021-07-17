2d8ed855e5724008f708b8236a258a0e6b768a5b
krishpranav <krisna.pranav@gmail.com>
2021-07-17 12:33:50 +0530

    src: main funcs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+extern crate hiddenparam;
+use colored::*;
+use reqwest::Client;
+use std::{
+    collections::HashMap,
+    fs,
+    io::{self, Write},
+    time::Duration,
+};
+use hiddenparam::{
+    args::get_config,
+    logic::cycles,
+    requests::{empty_reqs, random_request, request},
+    structs::Config,
+    utils::{compare, generate_data, heuristic, make_hashmap, random_line, read_lines, create_output},
+};
+
+#[cfg(windows)]
+#[tokio::main]
+async fn main() {
+    colored::control::set_virtual_terminal(true).unwrap();
+    run().await;
 }
+
+#[cfg(not(windows))]
+#[tokio::main]
+async fn main() {
+    run().await;
+}x
