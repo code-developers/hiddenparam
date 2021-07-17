5580f184afd75a0dc3bf150de998727f19e872bd
krishpranav <krisna.pranav@gmail.com>
2021-07-17 11:58:36 +0530

    struct: some public values

--- src/struct.rs
+++ src/struct.rs
@@ -13,3 +13,15 @@ pub struct FuturesData {
     pub remaining_params: Vec<String>
     pub found_params: Vec<String>
 }
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub method: String,
+    pub initial_url: String,
+    pub url: String,
+    pub host: String,
+    pub wordlist: String
+    pub body: String
+    pub proxy: String
+    pub replay_once: bool
+}
