b0d17f8b02bb28dc9c3e65f00b59a101642daea0
krishpranav <krisna.pranav@gmail.com>
2021-07-17 12:32:22 +0530

    src: utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,3 @@
-// use crates
 use crate::requests::request;
 use crate::structs::{Config, ResponseData};
 use crate::diff::diff;
@@ -38,7 +37,6 @@ lazy_static! {
         Regex::new(r#"(?P<first>"[\w\.-]*"):(?P<second>(false|null|true)),"#).unwrap();
 }
 
-//calls check_diffs & returns code and found diffs
 pub fn compare(
     initial_response: &ResponseData,
     response: &ResponseData,
@@ -51,7 +49,6 @@ pub fn compare(
         code = false
     }
 
-    //just push every found diff to the vector of diffs
     for diff in match diff(
         &initial_response.text,
         &response.text,
@@ -76,7 +73,6 @@ pub fn compare(
     (code, diffs)
 }
 
-//get possible parameters from the page code
 pub fn heuristic(body: &str) -> Vec<String> {
     let mut found: Vec<String> = Vec::new();
 
@@ -129,7 +125,7 @@ pub fn generate_request(config: &Config, initial_query: &HashMap<String, String>
             query_string.push_str(v);
             query_string.push('&');
         }
-        query_string.pop(); //remove the last &
+        query_string.pop(); 
 
         query_string = if config.encode {
             utf8_percent_encode(&query_string, &FRAGMENT).to_string()
@@ -197,14 +193,13 @@ pub async fn generate_data(config: &Config, client: &Client, query: &HashMap<Str
 pub fn adjust_body(body: &str, t: &str) -> String {
     let mut body = body.to_string();
 
-    //if type is json and body has parameters -> add an injection to the end
     if t.contains("json") && !body.is_empty() && body.contains('"') {
         body.pop();
         body.push_str(", %s}");
         body
     } else if t.contains("json") {
         String::from("{%s}")
-    //suppose that body type is urlencode
+
     } else if !body.is_empty()  {
         body.push_str("&%s");
         body
@@ -226,7 +221,7 @@ pub fn make_body(config: &Config, query: &HashMap<String, String>) -> String {
     }
 
     if config.body_type.contains("json") {
-        body.truncate(body.len().saturating_sub(2)) //remove the last ', '
+        body.truncate(body.len().saturating_sub(2)) 
     }
 
     body = match config.encode {
@@ -235,4 +230,210 @@ pub fn make_body(config: &Config, query: &HashMap<String, String>) -> String {
     };
 
     body
+}
+
+pub fn make_query(params: &HashMap<String, String>, config: &Config) -> String {
+    let mut query: String = String::from("");
+
+    for (k, v) in params {
+        query = query + &config.parameter_template.replace("%k", k).replace("%v", v);
+    }
+    query.pop();
+
+    if config.encode {
+        utf8_percent_encode(&query, &FRAGMENT).to_string()
+    } else {
+        query
+    }
+}
+
+pub fn make_hashmap(
+    words: &[String],
+    value_size: usize,
+) -> HashMap<String, String> {
+    let mut hashmap: HashMap<String, String> = HashMap::new();
+
+    for word in words.iter() {
+        let (param, value) = if word.matches('=').count() == 1 {
+            let mut splitted = word.split('=');
+            (splitted.next().unwrap(), splitted.next().unwrap().to_string())
+        } else {
+            (word.as_str(), "%random%_".to_owned()+&random_line(value_size))
+        };
+
+        hashmap.insert(param.to_string(), value);
+    }
+
+    hashmap
+}
+
+pub fn parse_request(proto: &str, request: &str, config: Config) -> Option<Config> {
+    let mut lines = request.lines();
+    let mut host = String::new();
+    let mut content_type = String::new();
+    let mut headers: HashMap<String, String> = config.headers.clone();
+    let mut firstline = lines.next()?.split(' ');
+    let method = firstline.next()?.to_string();
+    let mut path = firstline.next()?.to_string();
+
+    let http2: bool = firstline.next()?.to_string().contains("HTTP/2");
+
+    let mut parameter_template = match config.body_type == "json" {
+        true => String::from("\"%k\":\"%v\", "),
+        false => String::from("%k=%v&")
+    };
+
+    while let Some(line) = lines.next() {
+        if line.is_empty() {
+            break;
+        }
+
+        let mut k_v = line.split(':');
+        let key = k_v.next()?;
+        let value: String = [
+            k_v.next()?.trim().to_owned(),
+            k_v.map(|x| ":".to_owned() + x).collect(),
+        ].concat();
+
+        match key.to_lowercase().as_str() {
+            "content-type" => content_type = value.clone(),
+            "host" => {
+                host = value.clone();
+                if http2 {
+                    continue
+                }
+            },
+            "content-length" => continue,
+            _ => ()
+        };
+
+        headers.insert(key.to_string(), value);
+    }
+
+    let mut body = lines.next().unwrap_or("").to_string();
+    while let Some(part) = lines.next() {
+        if !part.is_empty() {
+            body.push_str(part);
+        }
+    }
+
+    let body_type = if config.body_type.contains('-') && config.as_body
+    && (content_type.contains("json") || (!body.is_empty() && body.starts_with('{'))) {
+        parameter_template = String::from("\"%k\":\"%v\", ");
+        String::from("json-")
+    } else {
+        config.body_type
+    };
+
+    let body = if config.as_body && ((!body.is_empty() && !body.contains("%s")) || body.is_empty()) {
+        adjust_body(&body, &body_type)
+    } else {
+        body
+    };
+
+    let mut url = [proto,"://", &host, &path].concat();
+    let initial_url = url.clone();
+    if !config.as_body && url.contains('?') && url.contains('=') && !url.contains("%s") {
+        url.push_str("&%s");
+        path.push_str("&%s");
+    } else if !config.as_body {
+        url.push_str("?%s");
+        path.push_str("?%s");
+    }
+
+    Some(Config {
+        method,
+        url,
+        host,
+        path,
+        headers,
+        body,
+        body_type,
+        parameter_template,
+        http2,
+        initial_url,
+        ..config
+    })
+}
+
+pub fn random_line(size: usize) -> String {
+    rand::thread_rng()
+        .sample_iter(&rand::distributions::Alphanumeric)
+        .take(size)
+        .collect::<String>()
+}
+
+pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+pub fn create_output(config: &Config, found_params: Vec<String>) -> String {
+    match config.output_format.as_str() {
+        "url" => {
+            let mut line = match config.initial_url.contains('?') {
+                true => config.initial_url.to_owned()+"&",
+                false => config.initial_url.to_owned()+"?"
+            };
+
+            for param in &found_params {
+                line.push_str(&param);
+                if !param.contains('=') {
+                    line.push('=');
+                    line.push_str(&random_line(config.value_size));
+                }
+                line.push('&')
+            }
+            line.pop();
+            line.push('\n');
+
+            line
+        }
+        "json" => {
+            let mut line = format!(
+                "{{\"method\":\"{}\", \"url\":\"{}\", \"parameters\":[",
+                &config.method,
+                &config.initial_url
+            );
+
+            for param in &found_params {
+                line.push('\"');
+                line.push_str(&param.replace("\"", "\\\""));
+                line.push('\"');
+                line.push_str(", ");
+            }
+
+            let mut line = line[..line.len() - 2].to_string();
+            line.push_str("]}\n");
+            line
+        },
+        _ => {
+            let mut line = format!("{} {} % ", &config.method, &config.initial_url);
+
+            for param in &found_params {
+                line.push_str(&param);
+                line.push_str(", ")
+            }
+
+            line = line[..line.len() - 2].to_string();
+            line.push('\n');
+            line
+        },
+    }
+}
+
+pub fn beautify_json(json: &str) -> String {
+    let json = json.replace("\\\"", "'");
+    let json = json.replace("\",", "\",\n");
+    let json = RE_JSON_BRACKETS.replace_all(&json, "${bracket}\n");
+    let json = RE_JSON_COMMA_AFTER_DIGIT.replace_all(&json, "$first:$second,\n");
+    let json = RE_JSON_COMMA_AFTER_BOOL.replace_all(&json, "$first:$second,\n");
+    json.to_string()
+}
+
+pub fn beautify_html(html: &str) -> String {
+    html.replace(">", ">\n")
 }
