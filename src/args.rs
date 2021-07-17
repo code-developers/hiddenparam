799aa3a3417e15e3fffbedf1d2c17c8eb99f1794
krishpranav <krisna.pranav@gmail.com>
2021-07-17 11:46:52 +0530

    src: args

--- src/args.rs
+++ src/args.rs
@@ -3,3 +3,27 @@ use clap::{crate_version, App, AppSettings, Arg};
 use std::{collections::HashMap, fs, time::Duration, io::{self, Write}};
 use url::Url;
 
+pub fn get_config() -> (Config, usize) {
+
+    let app = App::new("hiddenparam")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .version(crate_version!())
+        .author("krishpranav")
+        .about("Hidden parameters discovery suite")
+        .arg(Arg::with_name("url")
+            .short("u")
+            .long("url")
+            .help("You can add a custom injection point with %s")
+            .takes_value(true)
+            .conflicts_with("request")
+        )
+        .arg(Arg::with_name("request")
+            .short("r")
+            .long("request")
+            .help("The file with raw http request")
+            .takes_value(true)
+            .conflicts_with("url")
+        )
+
+    }
+}
